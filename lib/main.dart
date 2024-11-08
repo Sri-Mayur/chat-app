42515fa10fc31f920f2fd6d735a9feacb6735a7e
Mayur Srivastav <mayursrivastava877@gmail.com>
2024-11-08 19:13:45 +0530

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'May Chat',
+      title: 'Chat App',
       theme: lightTheme,
       darkTheme: darkTheme,
       themeMode: ThemeMode.dark,
