829b5bc6224927a42a8c956c66b2e29d820125cd
Mayur Srivastav <mayursrivastava877@gmail.com>
2024-11-08 18:44:59 +0530

    First Commit

---
+++ lib/Config/Colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const dBackgroundColor = Color(0xff191828);
+const dPrimaryColor = Color(0xff0057ff);
+const dContainerColor = Color(0xff343748);
+const dlOnBackgroundColor = Color(0xffFFFFFF);
+const dOnContainerColor = Color(0xff9E9E9E);
